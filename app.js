0fab1bb3be83861710862b778727f7aeaacc6e3b
Ivan Malenko <ivan.malenko18@gmail.com>
2023-06-10 16:05:12 +0300

    lesson 2

--- app.js
+++ app.js
@@ -7,3 +7,62 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+//  LESSON 2
+var age = 50;
+var userName = "John";
+var toogle = false;
+var empty = null;
+var notInitialize = undefined;
+var callback;
+callback = function (a) { return 100 + a; };
+var anything;
+anything = -20;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string') {
+    str = some;
+}
+var person;
+person = ['Max', 21];
+var LoadingStatus;
+(function (LoadingStatus) {
+    LoadingStatus[LoadingStatus["LOADING"] = 0] = "LOADING";
+    LoadingStatus[LoadingStatus["READY"] = 1] = "READY";
+})(LoadingStatus || (LoadingStatus = {}));
+var page = {
+    load: LoadingStatus.READY
+};
+page.load === LoadingStatus.LOADING ? console.log('Сторінка завантажується') : console.log('Сторінка завантажена');
+var someVariable;
+var anotherVariable;
+function showMessage(message) {
+    console.log(message);
+}
+showMessage("hello world!");
+
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+;
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: new Date('2021-01-01'),
+        updateAt: new Date('2021-05-01'),
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
--- app.ts
+++ app.ts
@@ -9,3 +9,102 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
 });
+
+//  LESSON 2
+
+let age: number = 50;
+let userName: string = "John";
+let toogle: boolean = false;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback: (a: number) => number;
+callback = (a) => { return 100 + a };
+ 
+let anything: any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
+
+
+let some:unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') { str = some; }
+
+
+
+let person: [string, number];
+person = ['Max', 21]
+
+
+enum LoadingStatus { LOADING, READY }
+
+const page = {
+  load: LoadingStatus.READY 
+}
+
+page.load === LoadingStatus.LOADING ? console.log('Сторінка завантажується') : console.log('Сторінка завантажена');
+
+
+
+let someVariable: string | number;
+let anotherVariable: 'enable' | 'disable';
+
+
+
+
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+showMessage("hello world!");
+
+function calc(num1:number, num2:number):number {
+  return num1 + num2;
+}
+
+function customError():never {
+  throw new Error('Error');
+};
+
+
+type pageTypes = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: "open" | "close",
+  details?: {createAt: Date, updateAt: Date}
+
+
+
+}
+
+const page1: pageTypes = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+
+
+const page2:pageTypes = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
+
+
+
+
+
+
